27c1ef3eca3a39ffdf11682e86f0c87cb4b219e6
Karthikan Jeyabalan <35607975+kj666@users.noreply.github.com>
2019-01-26 15:39:09 -0500

    Firesotre connected

--- app/src/main/java/com/example/billviewer/MainActivity.java
+++ app/src/main/java/com/example/billviewer/MainActivity.java
@@ -7,16 +7,39 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
 
     protected Button viewGradeButton = null;
     protected TextView textView = null;
+
+    public static final String RECEIPT_KEY = "name";
+    private CollectionReference receiptRef = FirebaseFirestore.getInstance().collection("receipt");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupUI();
+    }
+
+    public void fetchData(){
+        DocumentReference docRef = receiptRef.document("1");
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()){
+                    String receipt = documentSnapshot.getString(RECEIPT_KEY);
+                    textView.setText(receipt);
+                }
+            }
+        });
     }
 
     //Initialize the layout components
@@ -26,13 +49,15 @@ public class MainActivity extends AppCompatActivity {
 
         //Listen if the buttons is clicked
         viewGradeButton.setOnClickListener(onClickViewGradeButton);
+
+
     }
 
     private Button.OnClickListener onClickViewGradeButton = new Button.OnClickListener(){
 
         @Override
         public void onClick(View view) {
-            viewGrades();
+            fetchData();
         }
     };
 
