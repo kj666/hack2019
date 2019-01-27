8cd82a2fcbecc61af62708654b8ed0f41902174d
Karthikan Jeyabalan <35607975+kj666@users.noreply.github.com>
2019-01-27 02:50:44 -0500

    added pie chart

--- app/src/main/java/com/example/billviewer/Dashboard.java
+++ app/src/main/java/com/example/billviewer/Dashboard.java
@@ -1,17 +1,32 @@
 package com.example.billviewer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import lecho.lib.hellocharts.view.PieChartView;
 
 public class Dashboard extends AppCompatActivity {
 
     private float x1,x2,y1,y2;
+    protected Button viewGradeButton_dash = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        setupUI();
+
+        PieChartView pieChartView = findViewById(R.id.chart);
+    }
+
+    private void setupUI(){
+        viewGradeButton_dash = (Button) findViewById(R.id.viewAllBillsButton_dash);
+        viewGradeButton_dash.setOnClickListener(onClickViewGradeButton);
     }
 
     public boolean onTouchEvent(MotionEvent touchevent){
@@ -28,6 +43,22 @@ public class Dashboard extends AppCompatActivity {
                 break;
         }
         return false;
+    }
+
+
+    private Button.OnClickListener onClickViewGradeButton = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+//          fetchData();
+            viewBill();
+
+        }
+    };
+    private void viewBill(){
+        Intent intent = new Intent(this, ViewBill.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 
     public void finish(){
